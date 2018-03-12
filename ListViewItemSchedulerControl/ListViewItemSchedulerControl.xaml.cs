d2b38dc45207b9bf4c7eb3dc74604079a6e8a625
p1eXu5 <v1adp1exu5@gmail.com>
2018-03-12 22:35:23 +0300

    Добавлен ListViewItemSchedulerControl

---
+++ ListViewItemSchedulerControl/ListViewItemSchedulerControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+
+namespace ListViewItemSchedulerControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ListViewItemSchedule : UserControl
+    {
+        public ListViewItemSchedule()
+        {
+            InitializeComponent();
+        }
+    }
+}
